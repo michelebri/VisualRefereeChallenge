0034ddb84623d6b6957a214d0252527399fe0767
Michael Pio <michaelpio.stolfi@studenti.unibas.it>
2023-06-24 17:02:18 +0200

    α Added preprocessing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Detection import Detection
 from Homography import Homography
+from preprocessing import red_filtering, segmentation_and_cropping, equalizing, squaring
 import cv2
 from PIL import Image
 
@@ -15,27 +16,40 @@ from PIL import Image
 # 13: 'left_knee',      14: 'right_knee',
 # 15: 'left_ankle',     16: 'right_ankle'
 
-input_size = 192
-movenet = Detection(input_size)
-dst_point = {0: [160, 42], 1: [171, 31], 2: [145, 31], 3: [194, 34], 4: [121, 34], 5: [235, 111], 6: [93, 111],
-             7: [260, 201], 8: [70, 201], 9: [273, 290], 10: [54, 290], 11: [211, 301], 12: [120, 301],
-             13: [202, 425], 14: [107, 425], 15: [198, 529], 16: [103, 529]}
+# dst_point = {0: [160, 42], 1: [171, 31], 2: [145, 31], 3: [194, 34], 4: [121, 34], 5: [235, 111], 6: [93, 111],
+#              7: [260, 201], 8: [70, 201], 9: [273, 290], 10: [54, 290], 11: [211, 301], 12: [120, 301],
+#              13: [202, 425], 14: [107, 425], 15: [198, 529], 16: [103, 529]}
 
 webcam = cv2.VideoCapture(0)
 if not webcam.isOpened():
     raise Exception("Errore nell'apertura della webcam")
 
-frame_width = int(webcam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(webcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-pixel_to_rect = int((frame_width - frame_height) / 2)
-
+input_size = 192
+movenet = Detection(input_size)
 ret, image = webcam.read()
 while ret:
-    if ret == True:
-        image = image[0:frame_height, pixel_to_rect:(frame_width - pixel_to_rect)]
-        image = cv2.resize(image, (input_size, input_size))
-        keypoint_dict, out_im = movenet.inference(image, 0.4)
-        cv2.imshow("original", out_im)
+
+    image = cv2.flip(image, 1)
+    # -----------------------------PREPROCESSING START HERE-----------------------------
+    # RED FILTERING
+    full_mask = red_filtering(image)
+    # SEGMENTATION E CROPPING
+    cropped_image = segmentation_and_cropping(image, full_mask)
+    # NORMALIZATION
+    normalized_image = cv2.normalize(cropped_image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # EQUALIZATION
+    equalized_image = equalizing(normalized_image)
+    # SQUARING
+    squared_image = squaring(equalized_image)
+    # ------------------------------PREPROCESSING END HERE------------------------------
+
+    if (squared_image.shape[0] != 0) and (squared_image.shape[1] != 0) and (squared_image.shape[2] != 0):
+        # L'algoritmo di cropping se riceve una immagine senza nemmeno un pixel rosso può croppare troppo e
+        # annullare una dimensione
+
+        image = cv2.resize(squared_image, (input_size, input_size))
+        keypoint_dict, out_im = movenet.inference(image, 0.2)
+        cv2.imshow("Pose estimation", out_im)
 
         if bool(keypoint_dict):
             # -----------------------------HOMOGRAPHY START HERE-----------------------------
